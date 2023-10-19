5c59d218916287efdca335b25e007e5a8e03f025
beterali07 <148194890+beterali07@users.noreply.github.com>
2023-10-19 12:40:18 +0000

    sayılar kart denemeleri

---
+++ sayılar.js
@@ -0,0 +1,15 @@
+let kartIndex = 0;
+const kartlar = document.querySelectorAll('.kart');
+
+function kaydir() {
+    if (kartIndex < kartlar.length - 1) {
+        kartlar[kartIndex].style.transform = 'translateX(-100%)';
+        kartIndex++;
+    } else {
+        kartlar[kartIndex].style.transform = 'translateX(-100%)';
+        kartIndex = 0;
+    }
+}
+
+const kartContainer = document.querySelector('.kart-container');
+kartContainer.addEventListener('click', kaydir);

860e75474c24b6c21aff4bb92719c411ab3cbb4f
amrbinbeterali <154473942+amrbinbeterali@users.noreply.github.com>
2024-02-22 22:49:44 +0000

    Swp mench taslağı oluşturuldu katagoriler düzenleniyor

---
+++ swipenglish/Katagoriler/Sayılar/menchswipe.js
@@ -0,0 +1,77 @@
+function createCard(id, frontImageSrc, backImageSrc) {
+    const section = document.createElement('section');
+    section.innerHTML = `
+        <div>
+            <div class="card" id="${id}" onmousedown="startDragging(event, ${id})" onmouseup="stopDragging(${id})">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <img loading="eager" src="${frontImageSrc}">
+                    </div>
+                    <div class="card-back">
+                        <img loading="eager" src="${backImageSrc}">
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+    document.getElementById('main-container').appendChild(section);
+    setupDraggableCard(id);
+}
+
+function setupDraggableCard(cardId) {
+    let isDragging = false;
+    let initialX;
+    let initialY;
+
+    const card = document.getElementById(cardId);
+
+    function startDragging(event) {
+        isDragging = true;
+        initialX = event.clientX || event.touches[0].clientX;
+        initialY = event.clientY || event.touches[0].clientY;
+    }
+
+    function stopDragging() {
+        isDragging = false;
+    }
+
+    function handleDrag(event) {
+        if (isDragging) {
+            const deltaX = (event.clientX || event.touches[0].clientX) - initialX;
+            const deltaY = (event.clientY || event.touches[0].clientY) - initialY;
+            const threshold = 60;
+
+            // Check if the absolute deltaY is greater than the absolute deltaX
+            // If it is, ignore the drag event
+            if (Math.abs(deltaY) > Math.abs(deltaX)) {
+                return;
+            }
+
+            if (Math.abs(deltaX) > threshold || Math.abs(deltaY) > threshold) {
+                card.classList.toggle('flipped');
+                stopDragging();
+            }
+        }
+    }
+
+    card.addEventListener('mousedown', startDragging);
+    card.addEventListener('mouseup', stopDragging);
+    card.addEventListener('touchstart', startDragging);
+    card.addEventListener('touchend', stopDragging);
+    card.addEventListener('mousemove', handleDrag);
+    card.addEventListener('touchmove', handleDrag);
+}
+
+// Create cards  createCard('card', 'image/.webp', 'image/.webp');
+
+createCard('card1', 'image/zeroB.webp', 'image/zeroF.webp');
+createCard('card2', 'image/oneB.webp', 'image/oneF.webp');
+createCard('card3', 'image/twoB.webp', 'image/twoF.webp');
+createCard('card4','image/threeB.webp','image/threeF.webp')
+createCard('card5', 'image/fourB.webp', 'image/fourF.webp');
+createCard('card6', 'image/fiveB.webp', 'image/fiveF.webp'); 
+createCard('card7', 'image/sixB.webp', 'image/sixF.webp');
+createCard('card8', 'image/sevenB.webp', 'image/sevenF.webp');
+createCard('card9', 'image/eightB.webp', 'image/eightF.webp');
+createCard('card10', 'image/nineB.webp', 'image/nineF.webp');
+
